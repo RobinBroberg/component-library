29f677d6ddcd663c8a885042680c04f01e2dc994
RobinBroberg <robin.broberg86@gmail.com>
2025-01-14 01:37:14 +0100

    small change

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -2,14 +2,11 @@
 
 export default function CustomCard({ href, imgAlt, imgSrc, children }) {
   return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="rounded-lg shadow-md hover:shadow-lg border border-gray-400"
-    >
-      <img src={imgSrc} alt={imgAlt} className="rounded-t-lg" />
-      <div className="p-4">{children}</div>
-    </a>
+    <div className="rounded-lg shadow-md hover:shadow-lg border border-gray-300">
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <img src={imgSrc} alt={imgAlt} className="rounded-t-lg mx-auto" />
+        <div className="p-4">{children}</div>
+      </a>
+    </div>
   );
 }
